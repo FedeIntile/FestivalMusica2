397d87f13e3000ec8b4b9bf6c847b80fb87a4430
FedeIntile <fointile@gmail.com>
2024-09-02 09:41:39 -0300

    galeria de imagenes terminada

--- src/img/js/app.js
+++ src/img/js/app.js
@@ -31,25 +31,39 @@ function mostrarImagen(i){
   // generar el modal y se le agrega la imagen
   const modal = document.createElement('DIV'); // se crea el modal
   modal.classList.add('modal') // se le agrega una clase al modal
+  modal.onclick = cerrarModal // no hace falta pasarlo como funcion por que no toma parametros
+
+
+  // Botón cerrar modal
+  const cerrarModalBtn = document.createElement('BUTTON');
+  cerrarModalBtn.textContent = 'X'
+  cerrarModalBtn.classList.add('btn-cerrar')
+  cerrarModalBtn.onclick = cerrarModal
 
   modal.appendChild(imagen) // se le agrega la imagen
-
+  modal.appendChild(cerrarModalBtn) // se le agrega la imagen
     //cerrar el modal
-    modal.onclick = cerrarModal // no hace falta pasarlo como funcion por que no toma parametros
+ 
 
 
     // agregar al HTML
     const body = document.querySelector('body'); // selecciono el body
+    body.classList.add('overflow-hidden') // saca el scroll cuando seleccionamos una imagen
     body.appendChild(modal) // agrego al body el modal
 }
 
 function cerrarModal(){
     const modal = document.querySelector('.modal')
     modal.classList.add('fade-out')
+   
+   
 
     setTimeout(()=>{
         modal?.remove()// si existe el modal lo elimina 
-    },500); // settimeout retrasa la ejecucion 5 segundos 
+
+        const body = document.querySelector('body'); //seleciona el body
+        body.classList.remove('overflow-hidden'); // para luego eliminarlo       
+    }, 500); // settimeout retrasa la ejecucion 5 segundos 
     
 
     // if(modal){
